5b31e3ed163af81d08d6aea44f510d2a79656efe
Janaka Daskon <janakadaskon@gmail.com>
2023-12-12 20:47:50 +0530

    finalize home, about and menu sections

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,11 +5,11 @@ import { images } from '../../constants';
 
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
-    <div className='app__aboutus-overlay flex-center'>
+    <div className='app__aboutus-overlay flex__center'>
       <img src={images.G} alt="G_overlay" />
     </div>
 
-    <div className='app__aboutus-content flex_center '>
+    <div className='app__aboutus-content flex__center '>
       <div className='app__aboutus-content_about'>
         <h1 className='headtext__cormorant'>About Us</h1>
         <img src={images.spoon} alt="about_spoon" className='spoon__img' />
@@ -31,8 +31,12 @@ const AboutUs = () => (
       <div className='app__aboutus-content_history'>
         <h1 className='headtext__cormorant'>Our History</h1>
         <img src={images.spoon} alt="about_spoon" className='spoon__img' />
-        <p className='p__opensans'>....................</p>
-        <button type='button' className='custom__button'>Know More</button>
+        <p className='p__opensans'>Embark on a journey through time at Grandpa's Kitchen, where the 
+        rich tapestry of history intertwines with culinary mastery. Our story begins with Grandpa 
+        himself, a culinary virtuoso whose passion for cooking sparked the inception of our iconic 
+        restaurant. With roots deeply embedded in tradition, Grandpa's Kitchen is a living testament 
+        to the timeless recipes and flavors that have stood the test of generations.</p>
+        <button type='button' className='custom__button'>know more</button>
 
       </div>
     </div>
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's word"/>
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote_image" />
+          <p className='p__opensans'>Welcome to Grandpa's Kitchen, where every dish is a 
+          culinary masterpiece inspired by the heart and soul of our kitchen. As the chef behind 
+          these delectable creations, I am honored to share my passion for food and family traditions 
+          with each and every guest.
+
+          In our kitchen, we believe that a meal is not just a combination of ingredients; it's a 
+          journey through flavors, a dance of textures, and a celebration of culinary artistry. Each 
+          recipe has a story, and every dish is a testament to the time-honored techniques passed down 
+          through generations.
+
+          Our commitment to excellence goes beyond the plate. We meticulously source the finest, 
+          locally-sourced ingredients to ensure that every bite is an experience of freshness and 
+          authenticity. From the sizzle of the pan to the final presentation, every step is a labor 
+          of love.
+          </p>
+        </div>
+        <p className='p__opensans'>We are here to serve anything that you want</p>
+      </div>
+
+      <div className='app__chet-sign'>
+        <p>John Wick</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
   </div>
 );
 
